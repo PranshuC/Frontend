b61bf930e5ca3087369ecddf9d7c3b29b395aa07
Pranshu Chakraborty <pranshu.c1@gmail.com>
2024-02-24 14:58:36 +0530

    codesandbox setup in VS Code

---
+++ index.js
@@ -0,0 +1,16 @@
+// Data Types in JS
+
+// Primitive Data Types
+// String
+// Number
+// Boolean
+// Undefined
+// Null
+
+var str = "hello";
+var num = 2;
+var bool = true;
+
+console.log(str);
+console.log(num);
+console.log(bool);
